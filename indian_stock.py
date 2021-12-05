0a00df416e507f4e2a041c05e59dcb6fdff64d9a
Vishal Yadav <85288372+vishal-y@users.noreply.github.com>
2021-12-05 13:33:30 +0530

    Update indian_stock.py

--- indian_stock.py
+++ indian_stock.py
@@ -6,7 +6,7 @@ querystring = {"id":"500570"}
 
 headers = {
     'x-rapidapi-host': "indianstockexchange.p.rapidapi.com",
-    'x-rapidapi-key': "171b434de4msh9ce09d4c3be544fp1ff4b1jsnb8ae3c020020"
+    'x-rapidapi-key': "your_rapidapi_key_here"
     }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
