2fac6936c3dba2b86de745f66bec1f045e41e6a4
Vishal Yadav <85288372+vishal-y@users.noreply.github.com>
2021-12-05 13:35:06 +0530

    Update stock_api.py

--- stock_api.py
+++ stock_api.py
@@ -6,7 +6,7 @@ querystring = {"interval":"5min","function":"TIME_SERIES_INTRADAY","symbol":"msf
 
 headers = {
     'x-rapidapi-host': "alpha-vantage.p.rapidapi.com",
-    'x-rapidapi-key': "171b434de4msh9ce09d4c3be544fp1ff4b1jsnb8ae3c020020"
+    'x-rapidapi-key': "your_rapidapi_key_here"
     }
 
 got = requests.request("GET", link, headers=headers, params=querystring)
